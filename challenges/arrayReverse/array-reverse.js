e818ad6b19469b1377de12e40d6f91e57805c9da
JasonDormier <jmdormier@gmail.com>
2021-02-19 14:02:19 -0800

    fixed code challenges

--- challenges/arrayReverse/array-reverse.js
+++ challenges/arrayReverse/array-reverse.js
@@ -3,7 +3,8 @@
 function reverseArray(arr) {
   const result = [];
   for (let i = arr.length - 1; i >= 0; i--) {
-    result.push(arr[i]);
+    //result.push(arr[i]);
+    result[result.length] = arr[i];
   }
   return result;
 }
