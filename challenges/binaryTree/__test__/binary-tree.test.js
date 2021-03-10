e6c80be3d4ef4c386d56e3e0903fc38adcb37c35
JasonDormier <jmdormier@gmail.com>
2021-03-09 17:34:06 -0800

    challenge complete

--- challenges/binaryTree/__test__/binary-tree.test.js
+++ challenges/binaryTree/__test__/binary-tree.test.js
@@ -4,16 +4,24 @@ const binaryTree = require('../binary-tree.js');
 
 describe('Testing binary tree', () => {
 
+  const bt = new binaryTree.bT();
+  bt.root = new binaryTree.node(5);
+  bt.root.left = new binaryTree.node(6);
+  bt.root.right = new binaryTree.node(7);
+
 
   it('can successfully find the max number in the tree', () => {
-    const bt = new binaryTree.bT();
-    bt.root = new binaryTree.node(5);
-    bt.root.left = new binaryTree.node(6);
-    bt.root.right = new binaryTree.node(7);
 
     const max = bt.findMaxValue();
 
     expect(max).toEqual(7);
+
+  });
+  it('can successfully breadth first traverse the binary tree', () => {
+
+    const breadth = bt.breadthFirstTraversal();
+
+    expect(breadth).toEqual([5, 6, 7]);
 
   });
 
