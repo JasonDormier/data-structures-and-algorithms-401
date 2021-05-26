2251bea169b45e060e69bc007820cfb81899d6dc
JasonDormier <jmdormier@gmail.com>
2021-05-25 20:07:48 -0700

    ready to solve

---
+++ challenges/findLongestSubString/__tests__/findLongestSubString.test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const findLongestSubString = require('../findLongestSubString.js');
+
+describe('testing a function that returns the length of the longest substrng with all disctict characters', () => {
+  it('It should return a value of the unquie substring', () => {
+
+    const test = ('');
+    const test2 = ('rithmschool');
+    const test3 = ('thisisawesome');
+    const test4 = ('thecatinthehat');
+    const test5 = ('bbbbbbbb');
+    const test6 = ('longestsubstring');
+    const test7 = ('thisishowwedoit');
+
+    expect(test).toEqual(0);
+    expect(test2).toEqual(7);
+    expect(test3).toEqual(6);
+    expect(test4).toEqual(7);
+    expect(test5).toEqual(1);
+    expect(test6).toEqual(8);
+    expect(test7).toEqual(6);
+  });
+});
---
+++ challenges/findLongestSubString/findLongestSubString.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function longestSubString(str) {
+  //create varibles one to return and one for the window
+  //create an object to keep track of visited characters
+  //create for loop to loop through the string
+  //store str[i] in the object
+  //move the window Math.max
+  //index - beginning of substring +1
+  //store the index of the next str[i] so you don't double it
+  //return longest substring
+}
+
+module.exports = longestSubString;
