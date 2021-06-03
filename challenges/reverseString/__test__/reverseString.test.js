143684ef69e9f2375fea86934e60524537c2c45e
JasonDormier <jmdormier@gmail.com>
2021-06-03 10:56:12 -0700

    reverse strings complete

---
+++ challenges/reverseString/__test__/reverseString.test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const reverse = require('../reverseString.js');
+
+describe('Testing the three different functions to reverse a string', () => {
+    it('should reverse a string using spread and JavaScript methods', () => {
+
+       const test = reverse.reverse('happy')
+        expect(test).toEqual('yppah');
+    })
+    it('should reverse a string using a for loop', () => {
+
+        const test = reverse.reverse2('happy')
+         expect(test).toEqual('yppah');
+     })
+     it('should reverse a string using recursion', () => {
+
+        const test = reverse.reverse3('happy')
+         expect(test).toEqual('yppah');
+     })
+});
