4b257543b1cae64ab0cfb7142f207a25254bd1e8
JasonDormier <jmdormier@gmail.com>
2021-06-08 11:32:13 -0700

    more recursion challenges

---
+++ challenges/recursion/productOfArray.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function productOfArray(array){
+    if(array.length === 0) return 1;
+    return array[0] * productOfArray(array.slice(1));
+}
+
+module.exports = productOfArray;
---
+++ challenges/recursion/reverseString.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function reverseString(str){
+    if(str === '') return '';
+    return reverseString(str.substr(1)) + str[0];
+}
+
+module.exports = reverseString;
