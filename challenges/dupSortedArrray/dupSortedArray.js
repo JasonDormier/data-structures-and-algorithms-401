531667a0a78163d0f07f5dc50daa608154236a4f
JasonDormier <jmdormier@gmail.com>
2021-06-10 14:20:13 -0700

    adding array dups

---
+++ challenges/dupSortedArrray/dupSortedArray.js
@@ -0,0 +1,23 @@
+'use strict';
+
+//returns with mutated array
+function dups(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === array[i + 1]) {
+            array.splice(i ,1);
+            i --;
+        }
+    }
+    return array;
+}
+
+//returns with a new array
+function dupsNew(array) {
+    const temp = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== array[i + 1]) temp[temp.length] = array[i];
+    }
+    return temp;
+}
+console.log(dups([1, 2, 2, 2, 3, 3, 4, 5, 6, 7, 7, 7, 7, 7, 7, 7, 8]))
+console.log(dupsNew([1, 2, 2, 2, 3, 3, 4, 5, 6, 7, 7, 7, 7, 7, 7, 7, 8]))
