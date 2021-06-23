220dd6c31547c26234ec0816792893d3500a5a29
JasonDormier <jmdormier@gmail.com>
2021-06-23 15:11:35 -0700

    reverse int complete

--- challenges/reverseString/reverseString.js
+++ challenges/reverseString/reverseString.js
@@ -1,28 +1,26 @@
 'use strict';
 
-function stringReverse(str){
+function stringReverse(str) {
     return [...str].reverse().join('');
 }
 
-function reverseStringTwo(str){
+function reverseStringTwo(str) {
     let newString = '';
 
     for (let i = str.length - 1; i >= 0; i--) {
-        newString += str[i];   
+        newString += str[i];
     }
     return newString;
 }
 
-function reverseStringThree(str){
-    if(str === '') return '';
+function reverseStringThree(str) {
+    if (str === '') return '';
     return reverseStringThree(str.substr(1)) + str[0];
 
 }
 
-console.log(reverseStringThree('test'));
-
 module.exports = {
     'reverse': stringReverse,
     'reverse2': reverseStringTwo,
-    'reverse3':reverseStringThree
+    'reverse3': reverseStringThree,
 }
--- challenges/searches/binarySearch.js
+++ challenges/searches/binarySearch.js
@@ -1,21 +1,21 @@
 'use strict';
 
-function binarySearch(array, val){
+function binarySearch(array, val) {
     let a = 0;
-    let b = array.length -1;
-    let middle = Math.floor((a+b)/2);
-    while(array[middle] !== val && a <= b){
+    let b = array.length - 1;
+    let middle = Math.floor((a + b) / 2);
+    while (array[middle] !== val && a <= b) {
 
-        if(array[middle] < val){
+        if (array[middle] < val) {
             a = middle + 1;
-        }else{
-            b = middle -1; 
+        } else {
+            b = middle - 1;
         }
-        middle = Math.floor((a+b)/2);
+        middle = Math.floor((a + b) / 2);
     }
 
-    if(array[middle] === val) return middle;
+    if (array[middle] === val) return middle;
     else return -1;
 }
 
-console.log(binarySearch([1,2,3,4,5], 2));
+console.log(binarySearch([1, 2, 3, 4, 5], 2));
