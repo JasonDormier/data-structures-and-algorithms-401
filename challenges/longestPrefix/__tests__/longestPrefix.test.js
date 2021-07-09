6249f36ed377e0efa447cfffb371e227730e2663
JasonDormier <jmdormier@gmail.com>
2021-07-09 14:59:10 -0700

    adding tests

--- challenges/longestPrefix/__tests__/longestPrefix.test.js
+++ challenges/longestPrefix/__tests__/longestPrefix.test.js
@@ -2,8 +2,21 @@
 
 const longestPrefix = require('../longestPrefix');
 
-describe('', () =>{
-    it('', () => {
-        expect().toEqual();
+describe('testing a function that returns the matching prefix for each string in an array.', () => {
+    it('should return the matching letters that are the same for all words in the array.', () => {
+
+        const test = longestPrefix(["flower", "flow", "flight"]);
+        const test2 = longestPrefix(['a']);
+        expect(test).toEqual('fl');
+        expect(test2).toEqual('a');
+    })
+
+
+    it('should return an empty string if the array is empty', () => {
+
+        const test = longestPrefix(['']);
+        
+        expect(test).toEqual('');
+
     })
 })
