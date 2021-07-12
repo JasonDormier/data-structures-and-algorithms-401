3baf3970f8646e6a5adf31202679dc79b7fc3d59
JasonDormier <jmdormier@gmail.com>
2021-07-12 15:17:49 -0700

    challenge complete

---
+++ challenges/sumOfUnique/__tests__/sumOfUnique.test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const sumOfUnique = require('../sumOfUnique');
+
+describe('', () => {
+
+    it('should', () => {
+
+        const test = sumOfUnique([1, 2, 3, 2]);
+
+        expect(test).toEqual(4);
+
+    });
+});
