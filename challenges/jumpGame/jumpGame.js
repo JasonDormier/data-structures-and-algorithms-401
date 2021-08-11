ec383155ba3f15c962b8e2528c32bfb6600f00fd
JasonDormier <jmdormier@gmail.com>
2021-08-11 16:16:08 -0700

    different take on jump start

--- challenges/jumpGame/jumpGame.js
+++ challenges/jumpGame/jumpGame.js
@@ -7,14 +7,48 @@
 // try with a while loop and try with a for loop
 
 const jump = (nums) => {
-
+    let i = 0;
+    //let jumps = 1;
     let tracker = 0;
-    for (let i = 0; i < nums.length; i++) {
+
+    //if(nums.length === 1) return true;
+    //if(nums[i] === 0) return false;
+
+    while (i < nums.length) {
+        // console.log('========= round =========== ');
+        // tracker = nums[i];
+        // console.log(`tracker: ${tracker} + i: ${i} = ${nums[tracker + i]}`);
+        // i = tracker + i;
+        // if(nums[i] === 0){
+        //     i -= tracker;
+        //     console.log('i is 0');
+        //     i++;
+        // }
+        // console.log(`======= tracker: ${tracker} + i: ${i} = ${nums[tracker + i]}`);
+        // if(tracker + i >= nums.length -1) return true;
+        // console.log('+++', tracker)
+        // if(nums[tracker + i] === 0) return false;
+        // if(tracker === nums[nums.length] && i === nums.length) return false;
+        // jumps ++;
         if (i > tracker) return false;
         if (nums[i] + i > tracker) {
+            //console.log(`nums[i]: ${nums[i]} + i: ${i} = ${nums[i] + i}`);
             tracker = nums[i] + i;
         }
         if (tracker >= nums.length - 1) return true;
+
+        i++;
+        // for (let i = 0; i < nums.length; i++) {
+        //     console.log('========= round =========== ');
+        //     console.log('nums[i]:', nums[i]);
+        //     console.log('i: ', i);
+        //     console.log('tracker: ', tracker);
+        //     if (i > tracker) return false;
+        //     if (nums[i] + i > tracker) {
+        //         console.log(`nums[i]: ${nums[i]} + i: ${i} = ${nums[i] + i}`);
+        //         tracker = nums[i] + i;
+        //     }
+        //     if (tracker >= nums.length - 1) return true;
 
     }
     return false;
