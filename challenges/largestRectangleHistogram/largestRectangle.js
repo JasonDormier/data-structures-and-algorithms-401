7f4dfcfe165423ec9d9dea9337b489594f33da0c
JasonDormier <jmdormier@gmail.com>
2021-09-07 08:30:20 -0700

    code cleanup

--- challenges/largestRectangleHistogram/largestRectangle.js
+++ challenges/largestRectangleHistogram/largestRectangle.js
@@ -2,28 +2,25 @@
 
 const largestRectangle = (heights) => {
 
-    let maxArea = 0
+    let maxArea = 0;
     for (let i = 0; i < heights.length; i++) {
-        let width = 1
-        //console.log(`-----------${heights[i]}------------`);
+        let width = 1;
 
         //check all of the indexs infront of current height
         //if they are < current height keep adding to width
         for (let j = i + 1; j < heights.length; j++) {
-            if (heights[i] > heights[j]) break
-            //console.log('in j: ', heights[i]);
-            width++
+            if (heights[i] > heights[j]) break;
+            width++;
             
         }
         //check all of the indexs behing current height
         //if they are < current height keep adding to width
         for (let k = i-1; k >= 0; k--){
-            if(heights[i] > heights[k]) break
-            //console.log('in k: ', heights[i]);
-            width++
+            if(heights[i] > heights[k]) break;
+            width++;
         }
-        //multiply 
-        maxArea = Math.max(max, heights[i]* width);
+        //multiply current height * width 
+        maxArea = Math.max(maxArea, heights[i]* width);
     }
     return maxArea;
 }
