18d077d9004ff3bf03908c666bc3bf329c6cd280
JasonDormier <jmdormier@gmail.com>
2021-09-22 11:25:27 -0700

    challenge complete

--- challenges/spiralMatrix/spiralMatrix.js
+++ challenges/spiralMatrix/spiralMatrix.js
@@ -5,96 +5,95 @@ const spiralOrder = matrix => {
     const output = [];
 
     //top left value
-    let tl = matrix[0][0];
+    let tl = [0, 0];
     //top right value
-    let tr = matrix[0][matrix[0].length - 1];
-    //bottom left value
-    let br = matrix[matrix.length - 1][matrix[0].length - 1];
+    let tr = [0, matrix[0].length - 1];
     //bottom right value
-    let bl = matrix[matrix.length - 1][0];
+    let br = [matrix.length - 1, matrix[0].length - 1];
+    //bottom left value
+    let bl = [matrix.length - 1, 0];
 
-    let row = 0;
-    let col = 0;
+    let guide = [0, 0]
     let count = 0;
 
     const matrixSize = matrix.length * matrix[0].length;
 
     while (output.length <= matrixSize - 1) {
-        if (tl === tr && tl === bl && tl === br) {
-            output.push(matrix[row][col]);
+        if (tl[0] === br[0] && tl[1] === br[1]) {
+
+            output.push(matrix[guide[0]][guide[1]]);
         }
 
-        if (matrix[row][col] === tl) {
-            while (matrix[row][col] !== tr && output.length <= matrixSize - 1) {
-                output.push(matrix[row][col]);
-                col++;
+        if (guide[0] === tl[0] && guide[1] === tl[1]) {
+            while (guide[1] !== tr[1] && output.length <= matrixSize - 1) {
+                output.push(matrix[guide[0]][guide[1]]);
+                guide[1]++;
             }
         }
 
-        if (matrix[row][col] === tr) {
-            while (matrix[row][col] !== br && output.length <= matrixSize - 1) {
-                output.push(matrix[row][col]);
-                row++;
+        if (guide[0] === tr[0] && guide[1] === tr[1]) {
+            while (guide[0] !== br[0] && output.length <= matrixSize - 1) {
+                output.push(matrix[guide[0]][guide[1]]);
+                guide[0]++;
             }
         }
 
-        if (matrix[row][col] === br) {
-            while (matrix[row][col] !== bl && output.length <= matrixSize - 1) {
-                output.push(matrix[row][col]);
-                col--;
+        if (guide[0] === br[0] && guide[1] === br[1]) {
+            while (guide[1] !== bl[1] && output.length <= matrixSize - 1) {
+                output.push(matrix[guide[0]][guide[1]]);
+                guide[1]--;
             }
         }
 
-        if (matrix[row][col] === bl) {
-            while (matrix[row][col] !== tl && output.length <= matrixSize - 1) {
-                output.push(matrix[row][col]);
-                row--;
+        if (guide[0] === bl[0] && guide[1] === bl[1]) {
+            while (guide[0] !== tl[0] && output.length <= matrixSize - 1) {
+                output.push(matrix[guide[0]][guide[1]]);
+                guide[0]--;
             }
         }
         if (matrix.length >= 2) {
-            row++, col++, count++;
-            tl = matrix[0 + count][0 + count];
-            tr = matrix[0 + count][(matrix[0].length - 1) - count];
-            br = matrix[(matrix.length - 1) - count][(matrix[0].length - 1) - count];
-            bl = matrix[(matrix.length - 1) - count][0 + count];
+            guide[0]++, guide[1]++, count++;
+            tl = [0 + count, 0 + count];
+            tr = [0 + count, (matrix[0].length - 1) - count];
+            br = [(matrix.length - 1) - count, (matrix[0].length - 1) - count];
+            bl = [(matrix.length - 1) - count, 0 + count];
         }
-        console.log(output);
     }
     return output;
 }
 
-// console.log(spiralOrder([
-//     [1, 2, 3],
-//     [4, 5, 6],
-//     [7, 8, 9]
-// ])); //output [1,2,3,6,9,8,7,4,5]
+console.log(spiralOrder([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+])); //output [1,2,3,6,9,8,7,4,5]
 
-// console.log(spiralOrder([
-//     [1, 2, 3, 4],
-//     [5, 6, 7, 8],
-//     [9, 10, 11, 12]
-// ])); //output [1,2,3,4,8,12,11,10,9,5,6,7]
+console.log(spiralOrder([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]
+])); //output [1,2,3,4,8,12,11,10,9,5,6,7]
 
 console.log(spiralOrder([
     [1, 2, 3, 4, 5],
     [6, 7, 8, 9, 10],
-    [11, 22, 2, 14, 15],
+    [11, 12, 13, 14, 15],
     [16, 17, 18, 19, 20]
 ])); //output [1,2,3,4,5,10,15,20,19,18,17,16,11,6,7,8,9,14,13,12]
-// console.log(spiralOrder([
-//     [1, 2, 3],
-//     [4, 5, 6],
-//     [7, 8, 9],
-//     [10, 11, 12],
-//     [13, 14, 15],
-//     [16, 17, 18]
-// ])); //output [1,2,3,6,9,12,15,18,17,16,13,10,7,4,5,8,11,14]
-// console.log(spiralOrder([
-//     [1],
-// ]));
-// console.log(spiralOrder([
-//     [2, 3],
-// ]));
+console.log(spiralOrder([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11, 12],
+    [13, 14, 15],
+    [16, 17, 18]
+])); //output [1,2,3,6,9,12,15,18,17,16,13,10,7,4,5,8,11,14]
+console.log(spiralOrder([
+    [1],
+]));
+console.log(spiralOrder([
+    [2, 3],
+]));
 console.log(spiralOrder([
     [23, 18, 20, 26, 25],
     [24, 22, 3, 4, 4],
